076621fbcad72b754b1a4d9156c22f5a29bbb1b9
John Cusack <johncusackccm@gmail.com>
2018-05-02 01:30:01 +0000

    added mongoose

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
 const keys = require("./config/keys");
-
 require("./models/User");
 require("./services/passport");
 
@@ -23,5 +22,4 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, process.env.IP);
+app.listen(PORT);
