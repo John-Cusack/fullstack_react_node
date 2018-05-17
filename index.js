584ec24a3dc644a59fab9d6d2540e6e45921bc09
John Cusack <johncusackccm@gmail.com>
2018-05-17 17:51:37 +0000

    change build process

--- index.js
+++ index.js
@@ -24,5 +24,14 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
-const PORT = 8081 || 5000;
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = 8081 || process.env.PORT;
 app.listen(PORT);
